01345f9932689349dbb1a3c0ba5e77b0bc6282e0
Ajjack4 <manavjadhav1997@gmail.com>
2024-07-18 15:54:25 +0530

    fixed responsive issues

--- src/firebase.js
+++ src/firebase.js
@@ -3,12 +3,12 @@ import { initializeApp } from 'firebase/app';
 import { getFirestore, serverTimestamp } from 'firebase/firestore';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDb8UUS2B-mgDenPL8DH1L8TKvXiESisuA",
-  authDomain: "srct-link.firebaseapp.com",
-  projectId: "srct-link",
-  storageBucket: "srct-link.appspot.com",
-  messagingSenderId: "238395094241",
-  appId: "1:238395094241:web:8efa1de1b4341e540673a0",
+  apiKey: "YOUR_API_KEY",
+  authDomain: "YOUR_AUTH_DOMAIN",
+  projectId: "YOUR_PROJECT_ID",
+  storageBucket: "YOUR_STORAGE_BUCKET",
+  messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
+  appId: "YOUR_APP_ID",
   measurementId: "G-6XG4P1030R"
 };
 
